6279c28420aab0b146409e4ca977727e61c33d13
tminhbao <tminhbao2001@gmail.com>
2023-03-22 15:51:46 +0700

    feat(#t04): xử lí form và input cơ bản

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <AddTodo @add-todo="addTodo" />
     <TodoItem
       v-for="todo in todos"
       :key="todo.id"
@@ -13,9 +14,10 @@
 <script>
 import { ref } from "vue";
 import TodoItem from "./TodoItem.vue";
+import AddTodo from "./AddTodo.vue";
 export default {
   name: "Todo",
-  components: { TodoItem },
+  components: { TodoItem, AddTodo },
   setup() {
     const todos = ref([
       {
@@ -46,10 +48,15 @@ export default {
       todos.value = todos.value.filter((todo) => todo.id !== id);
     };
 
+    const addTodo = (newTodo) => {
+      todos.value.push(newTodo);
+    };
+
     return {
       todos,
       markCompleted,
       deleteTodo,
+      addTodo,
     };
   },
 };
